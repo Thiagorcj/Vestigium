8d0ddc82728daa8cd8fa0212829109f3b458e384
giselemenezes <128317764+giselemenezes@users.noreply.github.com>
2025-10-02 13:30:49 -0300

    Create download_dataset.py

---
+++ download_dataset.py
@@ -0,0 +1,21 @@
+import gdown
+import zipfile
+import os
+
+
+file_id = "1_P7Td9NzkV-7hj5O8k-C-OW7UGN5IjKC"
+url = f"https://drive.google.com/uc?id={file_id}"
+output = "dataset.zip"
+
+# Baixar 
+if not os.path.exists("dataset"):
+    print("Baixando dataset do Google Drive...")
+    gdown.download(url, output, quiet=False)
+
+    # Extrair dataset.zip
+    with zipfile.ZipFile(output, "r") as zip_ref:
+        zip_ref.extractall("dataset")
+    print("Dataset extraído em 'dataset/'")
+else:
+    print("Dataset já existe.")
+
